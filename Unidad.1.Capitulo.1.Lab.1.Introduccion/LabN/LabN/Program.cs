4250fde3597182d9a35e11e4536ce5cd50303804
Alejandro <alejandrorecalde5@gmail.com>
2021-03-25 18:49:39 -0300

    Complete lab

--- Unidad.1.Capitulo.1.Lab.1.Introduccion/LabN/LabN/Program.cs
+++ Unidad.1.Capitulo.1.Lab.1.Introduccion/LabN/LabN/Program.cs
@@ -6,7 +6,7 @@ namespace LabN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
