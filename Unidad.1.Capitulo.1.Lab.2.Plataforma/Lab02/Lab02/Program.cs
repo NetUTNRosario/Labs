b3c444771190d4e421878af411879c3ae078cfb6
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-03 02:11:09 -0300

    init unit 1 lab 2

---
+++ Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Lab02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
