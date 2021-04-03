3c71a5dec26a36376455caf35ac76d3bacd13635
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-03 02:25:47 -0300

    Complete lab

--- Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Lab02/Program.cs
+++ Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Lab02/Program.cs
@@ -6,7 +6,17 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string valor1 = "Este es el valor 1";
+            int valor2 = 5;
+            string valor3 = valor1;
+
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
         }
     }
 }
