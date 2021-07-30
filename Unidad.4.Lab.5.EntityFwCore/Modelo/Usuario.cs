66bc3f14a738b23cda1a2fd445e4cc16b1c6db65
ale <alejandrorecalde5@gmail.com>
2021-07-30 16:49:20 -0300

    Armar estructura inicial del lab ef core unidad 4

---
+++ Unidad.4.Lab.5.EntityFwCore/Modelo/Usuario.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Class1
+    {
+    }
+}
---
+++ Unidad.4.Lab.5.EntityFwCore/Repositorio/CursosRepositorio.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Repositorio
+{
+    public class CursosRepositorio
+    {
+    }
+}
