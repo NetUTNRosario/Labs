f4d01f0b71fee5cd58e07e75b4b07cc2351b54a4
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-08-02 20:44:27 -0300

    Implementar la consulta para obtener materias con ciertas caracteristicas

--- Unidad.4.Lab.5.EntityFwCore/DataAccess/CursosRepositorio.cs
+++ Unidad.4.Lab.5.EntityFwCore/DataAccess/CursosRepositorio.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Model;
 
 namespace DataAccess
 {
-    public class CursosRepository
+    public class CursosRepositorio
     {
+        private readonly Func<ApplicationContext> _contextBuilderFunction;
+
+        public CursosRepositorio(Func<ApplicationContext> contextBuilderFunc)
+        {
+            _contextBuilderFunction = contextBuilderFunc;
+        }
+
         /// Traer las materias con menos de x horas semanales con el plan z ordenados 
-        /// en forma descendente por HsSemanales
+        /// en forma descendente por HsSemanales, incluyendo los datos del plan y la 
+        /// especialidad asociados a esta
+        public IEnumerable<Materia> GetMaterias(int hsSemanales, int anioPlan)
+        {
+            using (var context = _contextBuilderFunction())
+            {
+                return context.Materias
+                    .Include(m => m.Plan).ThenInclude(p => p.Especialidad)
+                    .Where(m => m.HsSemanales <= hsSemanales && m.Plan.Anio == anioPlan)
+                    .OrderByDescending(m => m.HsSemanales).ToList();
+            }
+        }
     }
 }
