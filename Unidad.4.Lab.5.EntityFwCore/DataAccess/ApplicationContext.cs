6041eb91c6c5fc3f352f01915de4cc0d04438f8e
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-08-11 00:55:49 -0300

    Remover propiedades y constructor del application context

--- Unidad.4.Lab.5.EntityFwCore/DataAccess/ApplicationContext.cs
+++ Unidad.4.Lab.5.EntityFwCore/DataAccess/ApplicationContext.cs
@@ -3,11 +3,12 @@ using Model;
 
 namespace DataAccess
 {
+    // Crear las propiedades DbSet (las cuales representan tablas en la db) 
+    // para cada entidad del dominio (Materia, Planes y Especialidades).
+    // Luego crear constructor que tome DbContextOptions<ApplicationContext>
+    // como parametro y lo envié al constructor de la clase padre DbContext
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext> contextOptions): base(contextOptions) { }
-        public DbSet<Especialidad> Especialidades { get; set; }
-        public DbSet<Plan> Planes { get; set; }
-        public DbSet<Materia> Materias { get; set; }
+        
     }
 }
