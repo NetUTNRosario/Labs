f0aafe4e3295a5696473c1be5a937d4908e06650
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-08-31 15:50:01 -0300

    Generar controllador materia y accion list

---
+++ Unidad.5.Lab.1.MVC/Controllers/MateriaController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Unidad._5.Lab._1.MVC.Models;
+
+namespace Unidad._5.Lab._1.MVC.Controllers
+{
+    public class MateriaController : Controller
+    {
+        private readonly ILogger<MateriaController> _logger;
+        private readonly IMateriaRepository _materiaRepository;
+
+        public MateriaController(ILogger<MateriaController> logger, IMateriaRepository materiaRepository)
+        {
+            _logger = logger;
+            _materiaRepository = materiaRepository;
+            _logger.LogDebug("Inicializado controlador MateriaController");
+        }
+
+        public IActionResult List()
+        {
+            _logger.LogDebug("Acción: Listar materias");
+            return View(_materiaRepository.GetMaterias());
+        }
+    }
+}
