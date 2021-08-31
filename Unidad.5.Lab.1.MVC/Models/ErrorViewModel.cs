63ea3ca966a03453c027a1e0040c1ff2639432b0
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-08-31 14:00:09 -0300

    Init mvc lab

---
+++ Unidad.5.Lab.1.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Unidad._5.Lab._1.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
