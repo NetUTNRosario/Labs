d03c7bf883e8ccb1e6d4d2ee6db6898df4bc0f9d
Alejandro <alejandrorecalde5@gmail.com>
2021-09-07 00:06:10 -0300

    Agregar data annotations para validaciones en entidad materia

--- Unidad.5.Lab.1.MVC/Models/Materia.cs
+++ Unidad.5.Lab.1.MVC/Models/Materia.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
 namespace Unidad._5.Lab._1.MVC.Models
 {
     public class Materia
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string Descripcion { get; set; }
+        [Required]
+        [Range(2, 6)]
+        [Display(Name = "Horas Semanales")]
         public int HsSemanales { get; set; }
+        [Required]
+        [Range(90, 150)]
+        [Display(Name = "Horas Totales")]
         public int HsTotales { get; set; }
+        [Required]
         public int PlanId { get; set; }
         public Plan? Plan { get; set; }
     }
