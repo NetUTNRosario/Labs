43eed751b5027907226208bdca1d5b5fdeed0a87
Alejandro <alejandrorecalde5@gmail.com>
2021-09-14 14:16:13 -0300

    Utilizar fluent validation en vez de data annotations para validaciones

--- Unidad.5.Lab.1.MVC/Web/Models/Materia.cs
+++ Unidad.5.Lab.1.MVC/Web/Models/Materia.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -6,19 +7,23 @@ namespace Web.Models
     public class Materia
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 3)]
         public string Descripcion { get; set; }
-        [Required]
-        [Range(2, 6)]
         [Display(Name = "Horas Semanales")]
         public int HsSemanales { get; set; }
-        [Required]
-        [Range(90, 150)]
         [Display(Name = "Horas Totales")]
         public int HsTotales { get; set; }
-        [Required]
         public int PlanId { get; set; }
         public Plan? Plan { get; set; }
+    }
+
+    public class MateriaValidator: AbstractValidator<Materia>
+    {
+        public MateriaValidator()
+        {
+            RuleFor(m => m.Descripcion).NotEmpty().Length(min: 3, max: 20);
+            RuleFor(m => m.HsSemanales).NotEmpty().InclusiveBetween(from: 2, to: 6);
+            RuleFor(m => m.HsTotales).NotEmpty().InclusiveBetween(from: 90, to: 150);
+            RuleFor(m => m.PlanId).NotEmpty();
+        }
     }
 }
