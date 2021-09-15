4f67d5962d93d66cb92c5416bf00f8dc54b63156
Alejandro <alejandrorecalde5@gmail.com>
2021-09-15 17:07:08 -0300

    Usar un usuario manager para generar y adjuntar la auth cookie a la request

---
+++ Unidad.5.Lab.1.MVC/Web/Providers/UsuarioManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Providers
+{
+    public interface IUsuarioManager
+    {
+        Task SignIn(HttpContext httpContext, UsuarioLogeado usuarioLogeado, bool isPersistent = false);
+        Task SignOut(HttpContext httpContext);
+    }
+
+    public class UsuarioManager : IUsuarioManager
+    {
+        public async Task SignIn(HttpContext httpContext, UsuarioLogeado usuarioLogeado, bool isPersistent = false)
+        {
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, usuarioLogeado.Id.ToString()),
+                new(ClaimTypes.Name, usuarioLogeado.Nombre),
+                new(ClaimTypes.Email, usuarioLogeado.Mail),
+                new(ClaimTypes.Role, "Admin")
+            };
+
+            string authScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            var claimPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims, authScheme));
+
+            await httpContext.SignInAsync(authScheme, claimPrincipal, new AuthenticationProperties() { IsPersistent = isPersistent });
+        }
+
+        public async Task SignOut(HttpContext httpContext)
+        {
+            await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
