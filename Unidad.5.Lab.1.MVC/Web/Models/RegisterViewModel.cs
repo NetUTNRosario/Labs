a9921a2bdd9838de1b5f483e925ccd40f3f06e9a
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-09-16 00:01:18 -0300

    Remover implementación de RegisterValidator

--- Unidad.5.Lab.1.MVC/Web/Models/RegisterViewModel.cs
+++ Unidad.5.Lab.1.MVC/Web/Models/RegisterViewModel.cs
@@ -10,7 +10,6 @@ namespace Web.Models
         public string Nombre { get; set; }
         public string Mail { get; init; }
         public string Clave { get; init; }
-        [Display(Name = "Confirmar Clave")]
         public string ConfirmarClave { get; init; }
         public bool IsPersistent { get; init; }
     }
@@ -19,14 +18,6 @@ namespace Web.Models
     {
         public RegisterValidator()
         {
-            RuleFor(rvm => rvm.Nombre).Length(min: 3, max: 30);
-            RuleFor(rvm => rvm.Mail).NotEmpty().EmailAddress();
-            RuleFor(rvm => rvm.Clave).NotEmpty().MinimumLength(6)
-                // No se utiliza .Matches(@"\w*[A-Z]+\w*") ya que esto solo reconoce caracteres ASCII,
-                // es decir da errores de validacion al usar caracteres Unicode como 'Ñ' o 'ñ'
-                .Must(p => p.Any(Char.IsUpper)).WithMessage("La contraseña debe tener al menos un caracter en mayuscula")
-                .Must(p => p.Any(Char.IsNumber)).WithMessage("La contraseña debe tener al menos un numero");
-            RuleFor(rvm => rvm.ConfirmarClave).Equal(rvm => rvm.Clave);
         }
     }
 }
