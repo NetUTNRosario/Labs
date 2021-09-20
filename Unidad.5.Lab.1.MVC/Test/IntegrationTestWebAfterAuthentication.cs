18ae93f83c1f503c3be603aa447c1db1a33c8a3e
Alejandro <alejandrorecalde5@gmail.com>
2021-09-20 19:35:48 -0300

    Actualizar test de integracion para antes y despues de agregar autenticacion

--- Unidad.5.Lab.1.MVC/Test/IntegrationTestWebAfterAuthentication.cs
+++ Unidad.5.Lab.1.MVC/Test/IntegrationTestWebAfterAuthentication.cs
@@ -8,11 +8,11 @@ using Xunit;
 
 namespace Test
 {
-    public class IntegrationTestWeb : IClassFixture<WebApplicationFactory<Web.Startup>>
+    public class IntegrationTestWebAfterAuthentication : IClassFixture<WebApplicationFactory<Web.Startup>>
     {
         private readonly HttpClient _client;
 
-        public IntegrationTestWeb(WebApplicationFactory<Web.Startup> factory)
+        public IntegrationTestWebAfterAuthentication(WebApplicationFactory<Web.Startup> factory)
         {
             _client = factory.CreateClient();
         }
@@ -37,22 +37,33 @@ namespace Test
         }
 
         [Fact]
-        public async Task VisitCreateMateriaForm_ShouldDisplayThreeValidationSectionsAndTheSelectMustIncludeIngSistemasOption()
+        public async Task VisitCreateMateria_ShouldRedirectToLoginForm()
         {
-            // Arrange
-            var response = await _client.GetAsync("/Materia/Create");
-            /// Create a new context for evaluating webpages with the given config
-            var context = BrowsingContext.New(Configuration.Default);
+            await ProtectedActionRedirectsToLoginTest(inputUrl: "/Materia/Create", redirectedUrl: "http://localhost/Account/Login?ReturnUrl=%2FMateria%2FCreate");
+        }
 
+        [Fact]
+        public async Task VisitEditMateria_ShouldRedirectToLoginForm()
+        {
+            await ProtectedActionRedirectsToLoginTest(inputUrl: "/Materia/Edit/1", redirectedUrl: "http://localhost/Account/Login?ReturnUrl=%2FMateria%2FEdit%2F1");
+        }
+
+        [Fact]
+        public async Task VisitDeleteMateria_ShouldRedirectToLoginForm()
+        {
+            await ProtectedActionRedirectsToLoginTest(inputUrl: "/Materia/Delete/1", redirectedUrl: "http://localhost/Account/Login?ReturnUrl=%2FMateria%2FDelete%2F1");
+        }
+
+        private async Task ProtectedActionRedirectsToLoginTest(string inputUrl, string redirectedUrl)
+        {
             // Act
+            var response = await _client.GetAsync(inputUrl);
             var pageContent = await response.Content.ReadAsStringAsync();
-            var parsedDocument = context.GetService<IHtmlParser>().ParseDocument(pageContent);
 
             // Assert
             response.EnsureSuccessStatusCode();
-            Assert.Equal(actual: parsedDocument.QuerySelectorAll("span[class='text-danger field-validation-valid']").Length, expected: 3);
-            Assert.Contains(actualString: parsedDocument.QuerySelector("option[value='1']").TextContent
-                            , expectedSubstring: "Ingeniería en Sistemas de Información:2008");
+            Assert.Equal(actual: response.RequestMessage.RequestUri.AbsoluteUri, expected: redirectedUrl);
+            Assert.Contains("Password", pageContent);
         }
     }
 }
