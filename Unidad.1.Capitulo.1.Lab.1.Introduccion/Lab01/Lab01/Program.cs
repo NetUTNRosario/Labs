a20812f0d9eb42ba72debe45cc71033e9e20a8a4
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-03-28 12:27:41 -0300

    Crear "Hola Mundo"

---
+++ Unidad.1.Capitulo.1.Lab.1.Introduccion/Lab01/Lab01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundoooo");
+            Console.WriteLine("Pulse cualquier tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
