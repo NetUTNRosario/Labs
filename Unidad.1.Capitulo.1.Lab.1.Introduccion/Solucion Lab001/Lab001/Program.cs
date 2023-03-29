f2960524b72dd584456fdeaad350c1fbdb80e425
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-03-28 21:40:01 -0300

    Crear "Hola mundo" por tercera vez para probar GIT

---
+++ Unidad.1.Capitulo.1.Lab.1.Introduccion/Solucion Lab001/Lab001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundoooo, 3er intento git");
+            Console.WriteLine("Presione una tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
